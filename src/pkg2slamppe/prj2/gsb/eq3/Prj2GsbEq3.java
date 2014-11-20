fccb321b9d84054de77187c13778f30ba80d58fe
BaptisteDixneuf <baptiste.dixneuf@gmail.com>
2014-11-20 15:46:25 +0100

    first commit

---
+++ src/pkg2slamppe/prj2/gsb/eq3/Prj2GsbEq3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2slamppe.prj2.gsb.eq3;
+
+/**
+ *
+ * @author btssio
+ */
+public class Prj2GsbEq3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
