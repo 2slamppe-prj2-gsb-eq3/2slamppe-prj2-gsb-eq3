d2a1e270f0c7fcfc1ebe10c77c7ef393d90c9174
BaptisteDixneuf <baptiste.dixneuf@gmail.com>
2014-12-04 16:35:35 +0100

    Nettoyage

--- src/controleur/CtrlMenu.java
+++ src/controleur/CtrlMenu.java
@@ -15,28 +15,8 @@ public class CtrlMenu extends CtrlAbstrait {
         super(ctrlPrincipal);
         vue = new menu(this);
     }
-
-    /**
-     * clic sur la commande Quitter du menu Fichier Le contrôleur délègue
-     * l'action au contrôleur frontal
-     */
-    public void fichierQuitter() {
-        // Confirmer avant de quitter
-        int rep = JOptionPane.showConfirmDialog(getVue(), "Quitter l'application\nEtes-vous sûr(e) ?", "GSB", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-        if (rep == JOptionPane.YES_OPTION) {
-            // mettre fin à l'application
-            this.getCtrlPrincipal().action(EnumAction.MENU_FICHIER_QUITTER);
-        }
-    }
-
-    /**
-     * clic sur la commande Ajouter du menu presence Le contrôleur délègue
-     * l'action au contrôleur frontal
-     */
-    public void presenceAjouter() {
-        this.getCtrlPrincipal().action(EnumAction.MENU_PRESENCE_AJOUTER);
-    }
-
+    
+    
     @Override
     public menu getVue() {
         return (menu) vue;
