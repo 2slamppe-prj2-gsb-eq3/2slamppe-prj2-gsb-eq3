a374bbf6b58af76493af0d003e121d9ff2c77f4f
BaptisteDixneuf <baptiste.dixneuf@gmail.com>
2014-12-04 14:49:20 +0100

    not empty

--- src/modele/dao/metier.java
+++ src/modele/dao/metier.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package modele;
+package modele.dao;
 
 /**
  *
  * @author btssio
  */
-public class modele {
+public class metier {
     
 }
