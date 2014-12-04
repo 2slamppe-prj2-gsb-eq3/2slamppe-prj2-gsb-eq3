2daf79af5cc092fc6037b1035bdc4d621ef0cd92
btssio <btssio@btssio-VirtualBox>
2014-12-04 17:26:55 +0100

    ajout persistence

---
+++ src/modele/dao/Jdbc.java
@@ -0,0 +1,136 @@
+package modele.dao;
+
+import java.sql.*;
+
+/**
+ * Singleton fournit un objet de connexion JDBC
+ *
+ * @author nbourgeois
+ * @version 2 22 novembre 2013
+ */
+public class Jdbc {
+
+    // Instance du singleton Jdbc
+    private static Jdbc singleton = null;
+    // Paramètre de la connexion
+    private String piloteJdbc = "";
+    private String protocoleJdbc = "";
+    private String serveurBd = "";
+    private String nomBd = "";
+    private String loginSgbd = "";
+    private String mdpSgbd = "";
+    // Connexion
+    private Connection connexion = null; // java.sql.Connection
+
+    private Jdbc() {
+    }
+
+    /**
+     * @param pilote : classe du pilote Jdbc
+     * @param protocole : préfixe l'URL du serveur ; dépend du type de SGBD
+     * @param serveur : adresse du serveur + port (fini par un /, sauf pour
+     * Oracle ; BD pour embarquée : chemin accès répertoire )
+     * @param base : nom de la BD ou du DSN pour ODBC
+     * @param login : utilisateur autorisé du SGBD (ou schéma Oracle)
+     * @param mdp : son mot de passe
+     */
+    private Jdbc(String pilote, String protocole, String serveur, String base, String login, String mdp) {
+        this.piloteJdbc = pilote;
+        this.protocoleJdbc = protocole;
+        this.serveurBd = serveur;
+        this.nomBd = base;
+        this.loginSgbd = login;
+        this.mdpSgbd = mdp;
+    }
+
+    public static Jdbc creer(String pilote, String protocole, String serveur, String base, String login, String mdp) {
+        if (singleton == null) {
+            singleton = new Jdbc(pilote, protocole, serveur, base, login, mdp);
+        }
+        return singleton;
+    }
+
+    public static Jdbc getInstance() {
+        return singleton;
+    }
+
+    public void connecter() throws ClassNotFoundException, SQLException {
+        Class.forName(this.getPiloteJdbc());
+        setConnexion(DriverManager.getConnection(this.getProtocoleJdbc() + this.getServeurBd() + this.getNomBd(), this.getLoginSgbd(), this.getMdpSgbd()));
+        getConnexion().setAutoCommit(true);
+    }
+
+    public void deconnecter() throws SQLException {
+        getConnexion().close();
+    }
+
+    public static java.sql.Date utilDateToSqlDate(java.util.Date uneDate) {
+        return (new java.sql.Date(uneDate.getTime()));
+    }
+
+    /**
+     * ************************************* *
+     * ACCESSEURS * **************************************
+     */
+    public String getPiloteJdbc() {
+        return piloteJdbc;
+    }
+
+    public void setPiloteJdbc(String piloteJdbc) {
+        this.piloteJdbc = piloteJdbc;
+    }
+
+    /**
+     * @return the protocoleJdbc
+     */
+    public String getProtocoleJdbc() {
+        return protocoleJdbc;
+    }
+
+    /**
+     * @param protocoleJdbc the protocoleJdbc to set
+     */
+    public void setProtocoleJdbc(String protocoleJdbc) {
+        this.protocoleJdbc = protocoleJdbc;
+    }
+
+    public String getServeurBd() {
+        return serveurBd;
+    }
+
+    public void setServeurBd(String serveurBd) {
+        this.serveurBd = serveurBd;
+    }
+
+    public String getNomBd() {
+        return nomBd;
+    }
+
+    public void setNomBd(String nomBd) {
+        this.nomBd = nomBd;
+    }
+
+    public String getLoginSgbd() {
+        return loginSgbd;
+    }
+
+    public void setLoginSgbd(String loginSgbd) {
+        this.loginSgbd = loginSgbd;
+    }
+
+    public String getMdpSgbd() {
+        return mdpSgbd;
+    }
+
+    public void setMdpSgbd(String mdpSgbd) {
+        this.mdpSgbd = mdpSgbd;
+    }
+
+    public Connection getConnexion() {
+        return connexion;
+    }
+
+    public void setConnexion(Connection connexion) {
+        this.connexion = connexion;
+    }
+}
