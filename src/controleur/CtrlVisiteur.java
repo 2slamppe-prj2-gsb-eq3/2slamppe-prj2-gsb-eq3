88a098849f0a758990b5e442a74c8e8df4bf408b
BaptisteDixneuf <baptiste.dixneuf@gmail.com>
2014-12-11 16:01:09 +0100

    Liste Labo comboBox

--- src/controleur/CtrlVisiteur.java
+++ src/controleur/CtrlVisiteur.java
@@ -5,11 +5,13 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.swing.JOptionPane;
 import javax.swing.JOptionPane;
+import modele.dao.DaoLabo;
 import modele.dao.DaoVisiteur;
 //import modele.dao.*;
 //import modele.metier.*;
 import vues.VueVisiteurs;
 import modele.dao.EntityManagerFactorySingleton;
+import modele.metier.Labo;
 import modele.metier.Visiteur;
 
 /**
@@ -20,10 +22,13 @@ public class CtrlVisiteur extends CtrlAbstrait {
     
      EntityManager em;
      private List<Visiteur> lesVisteurs;
+     private List<Labo> lesLabos;
+     private VueVisiteurs vue = new  VueVisiteurs(this);
+     
 
     public CtrlVisiteur(CtrlPrincipal l) {
         super(l);
-        vue = new  VueVisiteurs(this);
+     //   vue = new  VueVisiteurs(this);
         
         // Gérer la persistance
         em = EntityManagerFactorySingleton.getInstance().createEntityManager();
@@ -33,7 +38,33 @@ public class CtrlVisiteur extends CtrlAbstrait {
         lesVisteurs = DaoVisiteur.selectAll(em);
         System.out.println(lesVisteurs);
         
+        lesLabos = DaoLabo.selectAll(em);
+        System.out.println(lesLabos);
+        afficherListeLabo(lesLabos);
+        
     }
+    
+    
+    /**
+     * Afficher la liste des labos
+     * 
+     */
+    public void afficherListeLabo(List<Labo> lesLabos){
+        for(int i=0; i<lesLabos.size(); i++ ){
+            vue.jComboBoxlabo.addItem(lesLabos.get(i).getNom());
+        }
+        
+    }
+
+    public VueVisiteurs getVue() {
+        return vue;
+    }
+
+    public void setVue(VueVisiteurs vue) {
+        this.vue = vue;
+    }
+    
+    
 
     
 
