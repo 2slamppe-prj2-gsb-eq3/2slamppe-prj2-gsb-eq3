3ceb2d303cd7d1b4895e29bd9696e1ad1b339c31
btssio <btssio@btssio-VirtualBox>
2014-12-11 15:27:31 +0100

    TestDaoVisiteur

---
+++ src/testDaoMetier/TestDaoVisiteur.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testDaoMetier;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import modele.dao.DaoVisiteur;
+import modele.dao.EntityManagerFactorySingleton;
+import modele.metier.Visiteur;
+
+/**
+ *
+ * @author btssio
+ */
+public class TestDaoVisiteur {
+
+    public static void main(String[] args) {
+        EntityManager em;
+        em = EntityManagerFactorySingleton.getInstance().createEntityManager();
+        em.getTransaction().begin();
+
+        //Test de selectAll
+        System.out.println("Test de selectAll : \n");
+        List<Visiteur> lesVisteurs = new ArrayList<Visiteur>();
+        lesVisteurs = DaoVisiteur.selectAll(em);
+        System.out.println("Les visiteurs sont : ");
+        for (int i = 0; i < lesVisteurs.size(); i++) {
+            System.out.println(lesVisteurs.get(i));
+        }
+        
+        //Test de selectOne
+        System.out.println("\nTest du selectOne : \n");
+        Visiteur unVisiteur;
+        String matricule = "a131";
+        unVisiteur = DaoVisiteur.selectOne(em, matricule);
+        System.out.println("Le visiteur qui a pour matricule : " + matricule + " est : \n" + unVisiteur);
+    }
+}
