c42b7681feaedd837cf72c0a4d520c3069ea57bf
BaptisteDixneuf <baptiste.dixneuf@gmail.com>
2015-03-05 14:52:33 +0100

    Ajout fichiers pour les fonctionnalités Rapport Visites

---
+++ src/controleur/CtrlRapportVisite.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controleur;
+
+/**
+ *
+ * @author btssio
+ */
+public class CtrlRapportVisite extends CtrlAbstrait {
+    
+    
+
+    public CtrlRapportVisite(CtrlPrincipal ctrlPrincipal) {
+        super(ctrlPrincipal);
+    }
+    
+}
